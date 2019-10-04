a1fd0c0bb26804d4369d76e27cd069cb3ee41d01
Mateus Gonçalves - 11819083 <11819083@animaedu.intranet>
2019-10-04 20:32:35 -0300

    Arquivo da aula 04/10

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Hello World</p>'; 
+    $numero =1;
+    echo $numero;
+    echo '<p>oi</p>';
+    define("ip","127.0.0.1");
+    echo ip;
+    $num1 =1;$num2 = 2;
+    echo("<br>A soma é ".($num1+$num2));
+    
+    
+    
+    
+    
+    
+    ?> 
+  </body>
+</html>
